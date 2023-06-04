53870f0920249d06812cb6acd41459ba587b40a5
freddyal <freddyal0x01@gmail.com>
2023-06-04 11:47:04 -0400

    Added logic for sending Solana from one wallet to another

---
+++ components/BalanceDisplay.tsx
@@ -0,0 +1,24 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { FC, useEffect, useState } from 'react';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+export const BalanceDisplay: FC = () => {
+    const { connection } = useConnection();
+	const { publicKey } = useWallet();
+
+    const [balance, setBalance] = useState(0);
+    
+    useEffect(() => {
+        if (!connection || !publicKey) { return }
+
+        connection.getAccountInfo(publicKey).then(info => {
+            setBalance(info.lamports)
+        })
+    }, [connection, publicKey]);
+
+    return (
+        <div>
+            <p>{publicKey ? `Balance: ${balance / LAMPORTS_PER_SOL} SOL` : ""}</p>
+        </div>
+    )
+}
--- components/SendSolForm.tsx
+++ components/SendSolForm.tsx
@@ -1,24 +1,61 @@
-import { FC } from 'react'
-import styles from '../styles/Home.module.css'
+import { FC, useState } from 'react';
+import { useConnection } from '@solana/wallet-adapter-react';
+import { useWallet } from '@solana/wallet-adapter-react';
+import * as web3 from "@solana/web3.js";
+import styles from '../styles/Home.module.css';
 
 
 export const SendSolForm: FC = () => {
 
+    const [txSig, setTxSig] = useState("");
+    const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
+    const link = () => {
+        return txSig ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet` : ""
+    }        
+
     const sendSol = event => {
-        event.preventDefault()
-        console.log(`Send ${event.target.amount.value} SOL to ${event.target.recipient.value}`)
+        event.preventDefault();
+        if (!connection || !publicKey) { return }
+
+        const transaction = new web3.Transaction();
+        const recipient = new web3.PublicKey(event.target.recipient.value)
+
+        const sendSolInstruction = web3.SystemProgram.transfer({
+            fromPubkey: publicKey,
+            toPubkey: recipient,
+            lamports: event.target.amount.value * web3.LAMPORTS_PER_SOL
+        });
+        
+        transaction.add(sendSolInstruction);
+
+        sendTransaction(transaction, connection).then(sig => {
+            setTxSig(sig)
+        })
     }
 
     return (
         <div>
-            <form onSubmit={sendSol} className={styles.form}>
-                <label htmlFor="amount">Amount (in SOL) to send:</label>
-                <input id="amount" type="text" className={styles.formField} placeholder="e.g. 0.1" required />
-                <br />
-                <label htmlFor="recipient">Send SOL to:</label>
-                <input id="recipient" type="text" className={styles.formField} placeholder="e.g. 4Zw1fXuYuJhWhu9KLEYMhiPEiqcpKd6akw3WRZCv84HA" required />
-                <button type="submit" className={styles.formButton}>Send</button>
-            </form>
+            {
+                publicKey ?
+                    <form onSubmit={sendSol} className={styles.form}>
+                        <label htmlFor="amount">Amount (in SOL) to send:</label>
+                        <input id="amount" type="text" className={styles.formField} placeholder="e.g. 0.1" required />
+                        <br />
+                        <label htmlFor="recipient">Send SOL to:</label>
+                        <input id="recipient" type="text" className={styles.formField} placeholder="e.g. 4Zw1fXuYuJhWhu9KLEYMhiPEiqcpKd6akw3WRZCv84HA" required />
+                        <button type="submit" className={styles.formButton}>Send</button>
+                    </form> :
+                    <span>Connect Your Wallet</span>
+            }
+            {
+                txSig ? 
+                    <div>
+                        <p>View your transaction on </p>
+                        <a href={link()}>Solana Explorer</a>
+                    </div> :
+                    null
+            }
         </div>
     )
 }
